69d77e1b8e5868f5ad6cc48b4299607f15ddcf6a
Mdlalose4life <blessing1059@gmail.com>
2023-09-22 08:26:43 +0200

    api

--- img/api/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// Auth routes
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/register', authController.registerUser);
-router.post('/login', authController.LoginUser);
-
-module.exports = router
--- img/api/routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const chatController = require('../controllers/chatController');
-
-router.get('/send', chatController.ShowChats);
-router.post('/send', chatController.sendMessage);
-
-
-module.exports = router;
